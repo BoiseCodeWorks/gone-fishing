471cbbccb3fa5aaf5eff48897ab664965ac1b986
Jake Overall <joverall22@gmail.com>
2019-06-04 11:11:26 -0600

    Update FishingService.js

--- app/components/fishing/FishingService.js
+++ app/components/fishing/FishingService.js
@@ -1,15 +1,10 @@
 // private
 // @ts-ignore
-const weatherApi = axios.create({
+const fishingApi = axios.create({
 	baseURL: "//bcw-sandbox.herokuapp.com/api/YOURNAME/fish",
 	timeout: 3000
 });
 
 // public
 export default class FishingService {
-
-  constructor() {
-
-  }
-
 }
